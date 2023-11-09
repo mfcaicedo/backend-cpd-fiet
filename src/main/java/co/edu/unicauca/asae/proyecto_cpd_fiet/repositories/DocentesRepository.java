9d8a1c3045408eaa2940d83345523ff8a749627c
Otaya443 <70723271+Otaya443@users.noreply.github.com>
2023-11-09 00:17:39 -0500

    Implements Repository
    All classes for repostories

---
+++ src/main/java/co/edu/unicauca/asae/proyecto_cpd_fiet/repositories/DocentesRepository.java
@@ -0,0 +1,8 @@
+package co.edu.unicauca.asae.proyecto_cpd_fiet.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.unicauca.asae.proyecto_cpd_fiet.models.DocentesEntity;
+
+public interface DocentesRepository extends JpaRepository<DocentesEntity, Integer> {
+}
---
+++ src/main/java/co/edu/unicauca/asae/proyecto_cpd_fiet/repositories/PublicacionesRepository.java
@@ -0,0 +1,10 @@
+package co.edu.unicauca.asae.proyecto_cpd_fiet.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import co.edu.unicauca.asae.proyecto_cpd_fiet.models.PublicacionesEntity;
+
+public interface PublicacionesRepository extends JpaRepository<PublicacionesEntity, Integer> {
+    
+}
