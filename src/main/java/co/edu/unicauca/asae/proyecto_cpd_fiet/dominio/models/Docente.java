1bdf7ecbf9254d63f264ad339eceed382067bdcb
mfcaicedo <mfcaicedo@unicauca.edu.co>
2023-11-16 22:51:28 -0500

    Solucion error al iniciar el proyecto

--- src/main/java/co/edu/unicauca/asae/proyecto_cpd_fiet/dominio/models/Docente.java
+++ src/main/java/co/edu/unicauca/asae/proyecto_cpd_fiet/dominio/models/Docente.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Docente extends Persona{
+public class Docente extends Persona {
     private String correo;
     private String vinculacion;
     private String departamento;
